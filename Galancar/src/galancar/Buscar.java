14888c443f5ceb85dbc4ee5eeedcd82638f95690
felipeparraaa <felipeparraaa@gmail.com>
2019-06-05 18:56:28 +0200

    Empezamos el JFrame Buscar

--- Galancar/src/galancar/Buscar.java
+++ Galancar/src/galancar/Buscar.java
@@ -5,7 +5,10 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.JButton;
 import javax.swing.ImageIcon;
 import java.awt.Font;
@@ -13,10 +16,12 @@ import javax.swing.SwingConstants;
 import javax.swing.JLabel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JComboBox;
 
 public class Buscar extends JFrame {
 
 	private JPanel contentPane;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -44,6 +49,32 @@ public class Buscar extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+
+			
+			table = new JTable();
+			table.setBounds(58, 13, 498, 160);
+			table.setModel(new DefaultTableModel(
+				new Object[][] {
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+					{null, null, null, null, null},
+				},
+				new String[] {
+					"N\u00BA DE VIAJE", "DNI CONDUCTOR", "ORIGEN", "DESTINO", "N\u00BA PLAZAS"
+				}
+			));
+			table.getColumnModel().getColumn(1).setPreferredWidth(112);
+			JScrollPane scrollPane = new JScrollPane(table);
+			scrollPane.setBounds(95, 213, 452, 193);
+			contentPane.add(scrollPane).setVisible(true);
+			getContentPane().add(scrollPane);
 		
 		JButton btnPublicar = new JButton("Publicar");
 		btnPublicar.addActionListener(new ActionListener() {
@@ -100,5 +131,29 @@ public class Buscar extends JFrame {
 		lblNewLabel.setIcon(new ImageIcon(Buscar.class.getResource("/imageresources/logosmall75.png")));
 		lblNewLabel.setBounds(557, 395, 75, 58);
 		contentPane.add(lblNewLabel);
+		
+		JComboBox comboBoxOrigen = new JComboBox();
+		comboBoxOrigen.setBounds(334, 71, 149, 20);
+		contentPane.add(comboBoxOrigen);
+		
+		JComboBox comboBoxDestino = new JComboBox();
+		comboBoxDestino.setBounds(159, 70, 149, 20);
+		contentPane.add(comboBoxDestino);
+		
+		JLabel lblOrigen = new JLabel("Origen");
+		lblOrigen.setFont(new Font("Arial", Font.BOLD, 14));
+		lblOrigen.setBounds(95, 72, 54, 14);
+		contentPane.add(lblOrigen);
+		
+		JLabel lblDestino = new JLabel("Destino");
+		lblDestino.setFont(new Font("Arial", Font.BOLD, 14));
+		lblDestino.setBounds(493, 73, 54, 14);
+		contentPane.add(lblDestino);
+		
+		JLabel lblenQuNumero = new JLabel("\u00BFEn qu\u00E9 numero de viaje quieres desplazarte?");
+		lblenQuNumero.setHorizontalAlignment(SwingConstants.CENTER);
+		lblenQuNumero.setFont(new Font("Arial", Font.BOLD, 14));
+		lblenQuNumero.setBounds(159, 146, 329, 14);
+		contentPane.add(lblenQuNumero);
 	}
 }
