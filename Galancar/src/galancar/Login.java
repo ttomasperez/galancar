d7880452f8fc427c480f3809ac8140838f06aa21
felipeparraaa <felipeparraaa@gmail.com>
2019-06-05 14:03:54 +0200

    HE BORRADO COMENTARIOS

--- Galancar/src/galancar/Login.java
+++ Galancar/src/galancar/Login.java
@@ -22,7 +22,7 @@ public class Login extends JFrame {
 	private JPasswordField passwordFieldContrasena;
 
 	/**
-	 * Launch the application. ME FUCKING GUSTA DAW dfgdfgdfgdfgdf HSDHFSDUFHSDUFHSDIUFHSDFUSDHFSUDFGHSIDUYF
+	 * Launch the application. ME FUCKING GUSTA DAW 
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
